2563c58982d6a10689f64efe3fec61acaadb56fc
Desmond Yao <jy2951@columbia.edu>
2019-11-30 18:55:46 -0500

    serial code committed

---
+++ dwt_serial.py
@@ -0,0 +1,39 @@
+import pywt
+import numpy as np
+
+signal = np.random.rand(100, 100)
+
+
+def gen_wavelet():
+    # Define the coefficients for the CDF9/7 filters
+    factor=1
+    cdf97_an_lo = factor*np.array([0, 0.026748757411, -0.016864118443, -0.078223266529, 0.266864118443,\
+                            0.602949018236, 0.266864118443, -0.078223266529,-0.016864118443, \
+                            0.026748757411])
+    cdf97_an_hi = factor*np.array([0, 0.091271763114, -0.057543526229,-0.591271763114,1.11508705,\
+                            -0.591271763114,-0.057543526229,0.091271763114,0, 0 ])
+    cdf97_syn_lo = factor*np.array([0, -0.091271763114,-0.057543526229,0.591271763114,1.11508705,\
+                             0.591271763114 ,-0.057543526229,-0.091271763114,0, 0])
+    cdf97_syn_hi = factor*np.array([0, 0.026748757411,0.016864118443,-0.078223266529,-0.266864118443,\
+                             0.602949018236,-0.266864118443,-0.078223266529,0.016864118443,\
+                             0.026748757411])
+    cdf97 = pywt.Wavelet('cdf97', [cdf97_an_lo,cdf97_an_hi,cdf97_syn_lo,cdf97_syn_hi])
+
+    wav = cdf97
+
+    # bior = pywt.Wavelet('db2')
+    # wav = bior
+
+    return wav
+
+def run_DWT(wav, mode='zero'):
+    a,d = pywt.dwt(signal,wav,mode)
+    print("approx: %s \n detail: %s \n" %(a,d))
+
+    return a, d
+
+
+def run_iDWT(wav, a, d, mode):
+    rec_sig = pywt.idwt(a,d,wav,mode)
+
+    return rec_sig
