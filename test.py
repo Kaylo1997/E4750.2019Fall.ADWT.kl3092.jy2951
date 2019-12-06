a4e1b4acc1060d96043be0156a066c30af4ee16e
Desmond Yao <jy2951@columbia.edu>
2019-12-06 04:10:38 +0000

    created the test function for naive parallel code

--- test.py
+++ test.py
@@ -3,14 +3,54 @@
 
 import numpy as np
 from dwt_serial import *
+from dwt_naive_parallel import *
 
 """
-1. Test with some random array
+1. Test serial with some random array
 """
-signal = np.random.rand(100, 100)
+signal = np.random.rand(2000, 2000).astype(np.float32)
 
 wav = gen_wavelet()
-cA, cH, cV, cD = run_DWT(signal, wav, True, mode='zero')
-rec_signal = run_iDWT(wav, cA, cH, cV, cD, mode='zero')
+cA, cH, cV, cD, serial_time = run_DWT(signal, wav, False, mode='zero')
+# rec_signal = run_iDWT(wav, cA, cH, cV, cD, mode='zero')
+#
+# print('Rec same as original: {}'.format(np.allclose(signal, rec_signal, atol=5e-7)))
 
-print('Rec same as original: {}'.format(np.allclose(signal, rec_signal)))
+"""
+2. Test parallel with some random array
+
+"""
+
+# Define the coefficients for the CDF9/7 filters
+factor = 1
+
+# FORWARD FILTER COEFFICIENTS
+# Forward Decomposition filter: lowpass
+cdf97_an_lo = factor * np.array([0, 0.026748757411, -0.016864118443, -0.078223266529, 0.266864118443,
+                                 0.602949018236, 0.266864118443, -0.078223266529, -0.016864118443,
+                                 0.026748757411])
+
+# Forward Decomposition filter: highpass
+cdf97_an_hi = factor * np.array([0, 0.091271763114, -0.057543526229, -0.591271763114, 1.11508705,
+                                 -0.591271763114, -0.057543526229, 0.091271763114, 0, 0])
+
+# INVERSE FILTER COEFFICIENTS
+# Inverse Reconstruction filter: lowpass
+cdf97_syn_lo = factor * np.array([0, -0.091271763114, -0.057543526229, 0.591271763114, 1.11508705,
+                                  0.591271763114, -0.057543526229, -0.091271763114, 0, 0])
+
+# Inverse Reconstruction filter: highpass
+cdf97_syn_hi = factor * np.array([0, 0.026748757411, 0.016864118443, -0.078223266529, -0.266864118443,
+                                  0.602949018236, -0.266864118443, -0.078223266529, 0.016864118443,
+                                  0.026748757411])
+filters = np.vstack((cdf97_an_lo, cdf97_an_hi, cdf97_syn_lo, cdf97_syn_hi)).astype(np.float32)
+dwt = DWT_naive()
+h_cA, h_cH, h_cV, h_cD, kernel_time = dwt.dwt_gpu_naive(signal, filters)
+
+print('Parallel same as serial c_A: {}'.format(np.allclose(cA, h_cA, atol=5e-7)))
+print('Parallel same as serial c_H: {}'.format(np.allclose(cH, h_cH, atol=5e-7)))
+print('Parallel same as serial c_V: {}'.format(np.allclose(cV, h_cV, atol=5e-7)))
+print('Parallel same as serial c_D: {}'.format(np.allclose(cD, h_cD, atol=5e-7)))
+
+print('\nSerial time: {}'.format(serial_time))
+print('Parallel time: {}'.format(kernel_time))
